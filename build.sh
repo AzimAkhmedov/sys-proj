2fbccedd028db934b123f1a24d3a2b227f9fe685
ziiyoda <ziiyodaergasheva@gmail.com>
2025-04-19 19:26:25 +0500

    Modernize file explorer UI and deleted navigation part

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-g++ -std=c++11 menu.cpp explorer.cpp main.cpp file_manager.cpp -D_GLIBCXX_USE_CXX11_ABI=0 -o menu `pkg-config --cflags --libs gtk+-3.0`
+# Compile with GTK+3 and additional dependencies for icons and styling
+g++ -std=c++11 menu.cpp explorer.cpp main.cpp file_manager.cpp -D_GLIBCXX_USE_CXX11_ABI=0 -o menu \
+    `pkg-config --cflags --libs gtk+-3.0` \
+    `pkg-config --cflags --libs gdk-3.0` \
+    `pkg-config --cflags --libs gio-2.0`
 
 if [ $? -ne 0 ]; then
     echo "Error occurred while compiling the code."
